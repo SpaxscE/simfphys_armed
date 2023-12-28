d35a45c06ed1cbd4f4be0c9720d4341a9c954083
Luna <swagnemite@gmx.de>
2023-12-28 19:18:49 +0100

    lvs_fakephysics=>simfphys, because the updater requires it

--- lua/autorun/simfphysarmed_init.lua
+++ lua/autorun/simfphysarmed_init.lua
@@ -1,15 +1,15 @@
 hook.Add( "LVS:ARMED:Initialize", "[LVS] - Armed [Fake Physics]", function()
 	if SERVER then
-		AddCSLuaFile("lvs_fakephysics/cl_armedvehicles_specialcam.lua")
-		AddCSLuaFile("lvs_fakephysics/cl_armedvehicles_tankextras.lua")
-		AddCSLuaFile("lvs_fakephysics/cl_armedvehicles_xhair.lua")
+		AddCSLuaFile("simfphys/cl_armedvehicles_specialcam.lua")
+		AddCSLuaFile("simfphys/cl_armedvehicles_tankextras.lua")
+		AddCSLuaFile("simfphys/cl_armedvehicles_xhair.lua")
 
-		include("lvs_fakephysics/sv_armedvehicles_handler.lua")
+		include("simfphys/sv_armedvehicles_handler.lua")
 
 		return
 	end
 
-	include("lvs_fakephysics/cl_armedvehicles_specialcam.lua")
-	include("lvs_fakephysics/cl_armedvehicles_tankextras.lua")
-	include("lvs_fakephysics/cl_armedvehicles_xhair.lua")
+	include("simfphys/cl_armedvehicles_specialcam.lua")
+	include("simfphys/cl_armedvehicles_tankextras.lua")
+	include("simfphys/cl_armedvehicles_xhair.lua")
 end)
